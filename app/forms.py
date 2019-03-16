51f7332bbb8c2ef055418a09bb8c33164a6bf543
Kayon Marie Douglas <kayonmarie.douglas@mymona.uwi.edu>
2019-03-16 17:09:44 +0000

    Display profiles within the database on the profiles.html page

--- app/forms.py
+++ app/forms.py
@@ -7,8 +7,8 @@ from wtforms.validators import DataRequired, Email
 class ProfileForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
-    gender = SelectField('Gender', choices=[('female','Female'), ('male','Male')])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    gender = SelectField('Gender', choices=[('Female','female'), ('Male','male')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     location = StringField('Location', validators=[DataRequired()])
     bio = TextAreaField("Biography", validators=[DataRequired()])
     photo = FileField('Profile Picture', validators=[FileRequired(), FileAllowed(['jpg','png','jpeg'], 'Images only!')])
