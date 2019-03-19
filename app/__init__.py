1cc9d3bd140de48dc017c3109c509f60d090a2fb
Kayon Marie Douglas <kayonmarie.douglas@mymona.uwi.edu>
2019-03-19 00:04:59 +0000

    Updated init with new databse uri

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Su24p3r$3cr9et0key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:password123@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://xgnoajsclwmuib:b52ed944b47ea3d16a8d76b5af5ac5513102d2c4ef43cc13c0e7a69e5621f273@ec2-107-20-177-161.compute-1.amazonaws.com:5432/dbhjgd4v52okf4"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 
